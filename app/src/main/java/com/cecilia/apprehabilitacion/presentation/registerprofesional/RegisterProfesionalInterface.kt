a97497dfe512c39aa9cd47a290cc1017466c36fe
Cecilia <cecimanoni@gmail.com>
2022-03-17 10:23:27 -0300

    version superp

--- app/src/main/java/com/cecilia/apprehabilitacion/presentation/registerprofesional/RegisterProfesionalInterface.kt
+++ app/src/main/java/com/cecilia/apprehabilitacion/presentation/registerprofesional/RegisterProfesionalInterface.kt
@@ -15,7 +15,7 @@ interface RegisterProfesionalInterface {
         fun detachView()
         fun isViewAttached():Boolean
         fun singUp(fullname:String, email:String, password:String)
-        fun checkEmptyFields(email:String, password:String):Boolean
+        fun checkEmptyFields(password:String):Boolean
         fun checkValidEmail(email: String):Boolean
         fun checkEmptyPassword(pw1:String, pw2:String):Boolean
         fun checkPasswordMatch(pw1: String, pw2:String):Boolean
